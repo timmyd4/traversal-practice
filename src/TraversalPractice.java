3360b8d677012bd7f3cb5c3ddbe7c206c6be2baa
Tim <timmywilliams4665@gmail.com>
2025-05-02 08:37:48 -0700

    Passed all tests

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -94,7 +94,13 @@ public class TraversalPractice {
    * @return The number of levels in the tree
    */
   public static <T> int numLevels(Node<T> node) {
-    return 0;
+
+    if(node == null) return 0;
+
+    int left = numLevels(node.left);
+    int right = numLevels(node.right);
+
+    return 1 + Math.max(left, right);
   }
 
   public static void main(String[] args) {
