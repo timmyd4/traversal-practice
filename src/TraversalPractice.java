5e98983d741a131a51f144ed57406e33dc810f4f
Tim <timmywilliams4665@gmail.com>
2025-05-02 00:07:40 -0700

    Passed maxVal test

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -75,7 +75,13 @@ public class TraversalPractice {
    * @return the max value
    */
   public static int maxVal(Node<Integer> node) {
-    return 0;
+
+    if (node == null) return 0;
+
+    int leftMax = maxVal(node.left);
+    int rightMax = maxVal(node.right);
+
+    return Math.max(node.value, Math.max(leftMax, rightMax));
   }
 
   /**
