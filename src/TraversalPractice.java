e8e79310520245577453b3d6bc06a85c58b28692
Tim <timmywilliams4665@gmail.com>
2025-05-01 23:45:04 -0700

    Passed nodesWithOneChild test

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -34,6 +34,19 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static <T> void printNodesWithOneChild(Node<T> node) {
+
+    if(node == null) return;
+
+    if(node.left == null && node.right != null)
+    {
+      System.out.println(node.value);
+    }  
+    else if(node.left != null && node.right == null)
+    {
+      System.out.println(node.value);
+    }
+    printNodesWithOneChild(node.left);
+    printNodesWithOneChild(node.right);
     
   }
 
